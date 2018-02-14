57304e04dd55a3b8dea5414a355532205766e44e
benrhine <rhine.ben@gmail.com>
2018-02-14 15:40:04 -0500

    minor doc update

--- complete/grails-app/tasks/demo/EmailTask.groovy
+++ complete/grails-app/tasks/demo/EmailTask.groovy
@@ -1,17 +1,17 @@
 package demo
 
-class EmailTask implements Runnable {
+class EmailTask implements Runnable { //<1>
 
-    String email
-    EmailService emailService
+    String email //<2>
+    EmailService emailService //<3>
 
-    EmailTask(EmailService emailService, String email){
+    EmailTask(EmailService emailService, String email) { //<4>
         this.emailService = emailService
         this.email = email
     }
 
     @Override
-    void run() {
+    void run() { //<5>
         emailService.send(email)
     }
 }
