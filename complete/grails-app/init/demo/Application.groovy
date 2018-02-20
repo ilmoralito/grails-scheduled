abedfa68d75f0a4ba98d22f883c0024245adef51
Alvaro Sanchez-Mariscal <amariscal@rgare.com>
2018-02-20 10:34:02 +0000

    Use a proper @Configuration class so that @Bean definitions are actually executed

--- complete/grails-app/init/demo/Application.groovy
+++ complete/grails-app/init/demo/Application.groovy
@@ -3,8 +3,10 @@ package demo
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 import groovy.transform.CompileStatic
+import org.springframework.context.annotation.ComponentScan
 
 @CompileStatic
+@ComponentScan('demo')
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
